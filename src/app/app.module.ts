6071116da80f901e221a53a2331cdca9e190aaee
ashu95 <ghildiyal.a@husky.neu.edu>
2018-08-07 04:22:08 -0400

    Log In Client Side

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { WhiteBoardComponent } from './white-board/white-board.component';
 import {CourseServiceClient} from '../services/course.service.client';
 import { CourseNavigatorComponent } from './course-navigator/course-navigator.component';
 import {routing} from './app.routing';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WhiteBoardComponent,
-    CourseNavigatorComponent
+    CourseNavigatorComponent,
+    RegisterComponent,
+    ProfileComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     routing
   ],
   providers: [
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,14 @@
 import {Routes, RouterModule} from '@angular/router';
 import {CourseNavigatorComponent} from './course-navigator/course-navigator.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {ProfileComponent} from './profile/profile.component';
 
 const appRoutes: Routes = [
-  {path: 'courses', component: CourseNavigatorComponent}
+  {path: 'courses',  component:    CourseNavigatorComponent},
+  {path: 'login',    component:    LoginComponent},
+  {path: 'register', component:    RegisterComponent},
+  {path: 'profile',  component:    ProfileComponent}
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
