4801ec13625d1e61e62019db96ef51124acf6ea6
ashu95 <ghildiyal.a@husky.neu.edu>
2018-08-09 01:24:07 -0400

    section services modified

--- src/services/section.service.client.ts
+++ src/services/section.service.client.ts
@@ -1,28 +1,80 @@
-const SECTION_API_URL = 'http://localhost:3000/api'
+const SECTION_API_URL = 'http://localhost:3000/api';
 
 import {Injectable} from '@angular/core';
 
 @Injectable()
 export class SectionServiceClient {
   createSection(section) {
-   return fetch ((SECTION_API_URL + '/section'), {
+    return fetch((SECTION_API_URL + '/course/' + section.courseId + '/section'), {
       method: 'POST',
       body: JSON.stringify(section),
       credentials: 'include',
       headers:
-        {'Content-Type' : 'application/json'}
+        {'Content-Type': 'application/json'}
     }).then(function (response) {
       return response.json();
 
     });
   }
 
-
-  findSectionsForCourse(courseId) {
-    return fetch ((SECTION_API_URL + '/course/' + courseId + '/section'), {
+  deleteSection(sectionId) {
+    return fetch((SECTION_API_URL + '/section/' + sectionId), {
+      method: 'DELETE',
       credentials: 'include'
     }).then(function (response) {
-        return response.json();
-      });
+      return response;
+    });
+  }
+
+  updateSection(section){
+    return fetch ((SECTION_API_URL + '/section/' + section.sectionId), {
+      credentials: 'include',
+      method : 'PUT',
+      body : JSON.stringify(section),
+      headers:
+        {'content-type' : 'application/json'}
+    }).then(function (response) {
+      return response;
+    });
+  }
+
+  findSectionsForCourse(courseId) {
+    return fetch((SECTION_API_URL + '/course/' + courseId + '/section'))
+      .then(function (response) {
+      return response.json();
+    });
+  }
+
+  findAllSections() {
+    return fetch((SECTION_API_URL + '/findAllSections'),
+      {
+        credentials: 'include'
+      }).then(function (response) {
+      return response.json();
+    });
+
+  }
+
+  enrollSection(sectionId) {
+    return fetch((SECTION_API_URL + '/section/enroll/' + sectionId), {
+      credentials: 'include',
+      method: 'PUT'
+    }).then(function (response) {
+      return response;
+    });
+  }
+
+  unenrollSection(sectionId) {
+    return fetch((SECTION_API_URL + '/section/unenroll' + sectionId), {
+      credentials: 'include',
+      method: 'delete'
+    });
+  }
+
+  findSectionsForStudent() {
+    return fetch (SECTION_API_URL + '/student/section', {
+      credentials: 'include'
+    }).then(response => response.json());
   }
 }
+
