e44488f2db14c8a923a5675a06dfd4b0d9dd4550
ashu95 <ghildiyal.a@husky.neu.edu>
2018-08-09 01:23:35 -0400

    findCourseById in course-services

--- src/services/course.service.client.ts
+++ src/services/course.service.client.ts
@@ -1,6 +1,5 @@
 const COURSE_API_URL = 'https://webdev-2.herokuapp.com/api/course';
 
-
 import {Injectable} from '@angular/core';
 
 @Injectable()
@@ -8,6 +7,11 @@ export class CourseServiceClient {
   findAllCourses() {
     return fetch(COURSE_API_URL)
       .then(response => response.json());
+  }
+
+  findCourseById(courseId){
+    return fetch (COURSE_API_URL + '/findCourse/' + courseId)
+      .then (response => response.json());
   }
 }
 
