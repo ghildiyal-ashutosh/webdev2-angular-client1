e0ea7338ca8c2e9d021ddd37bfdb3808c236977b
ashu95 <ghildiyal.a@husky.neu.edu>
2018-08-10 05:22:40 -0400

    Routing changes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -5,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/webdev2-angular-ashu95'));
+app.use(express.static(__dirname + '/dist/webdev2-angular-client1'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname,'/dist/webdev2-angular-ashu95/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/webdev2-angular-client1/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
